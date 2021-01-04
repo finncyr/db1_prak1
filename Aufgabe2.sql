7dbbbe5407e677fe88e4b6922a2e4b48aa639ce2
Finn Cyriax <finn_christian.cyriax@smail.th-koeln.de>
2021-01-05 00:57:52 +0100

    query 6

--- Aufgabe2.sql
+++ Aufgabe2.sql
@@ -6,6 +6,11 @@ FROM
 WHERE
     VorlNr = 5041;
 
+-- Ergebnis
+-- "matrnr"
+-- 28106
+-- 29120
+----------------------------------------------------------------------
 -- Query 2
 select
     distinct Studenten.name
@@ -43,4 +48,83 @@ where
             name = 'Schopenhauer'
     );
 
+-- Ergebnis
+-- "name"
+-- "Theophrastos             "
+-- "Fichte                   "
+-- "Feuerbach                "
+---------------------------------------------------------
+-- Query 2 & 3 identical?
+---------------------------------------------------------
+-- Query 4
+select
+    nachfolger
+from
+    voraussetzen
+group by
+    nachfolger
+having
+    count(nachfolger) >= 2;
+
+-- Ergebnis
+-- "nachfolger"
+-- 5052
+---------------------------------------------------------
+-- Query 5
+select
+    Vorlnr,
+    count(*) as Anzahl
+from
+    pruefen
+group by
+    vorlnr
+order by
+    vorlnr asc;
+
+-- Ergebnis
+-- "vorlnr","anzahl"
+-- 4630,1
+-- 5001,1
+-- 5041,1
+---------------------------------------------------------
+-- Query 6
+select
+    name
+from
+    professoren
+where
+    persnr in (
+        select
+            boss
+        from
+            (
+                select
+                    count(*) as Anzahl,
+                    boss
+                from
+                    assistenten
+                group by
+                    boss
+            ) as t2
+        where
+            t2.Anzahl = (
+                select
+                    max(Anzahl) as MaxAnzahl
+                from
+                    (
+                        select
+                            count(*) as Anzahl,
+                            boss
+                        from
+                            assistenten
+                        group by
+                            boss
+                    ) as t1
+            )
+    )
+
+-- Ergebnis
+-- "name"
+-- "Sokrates"
+-- "Kopernikus"
 
